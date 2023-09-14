08b1e72a6188bb81764492b4c7a3805d7a8dc52e
SerialQi <chengxuanmei@gmail.com>
2023-09-14 15:43:30 +1200

    FINISHED!!!

--- my_phenakistoscope.js
+++ my_phenakistoscope.js
@@ -1,7 +1,7 @@
 const SLICE_COUNT = 9;//分割数量
 
 function setup_pScope(pScope){
-  //pScope.output_mode(OUTPUT_GIF(1000));
+  //pScope.output_mode(OUTPUT_PRINT(A3));
   pScope.output_mode(ANIMATED_DISK);
   pScope.scale_for_screen(true);
   pScope.draw_layer_boundaries(false);
